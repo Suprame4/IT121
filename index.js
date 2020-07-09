c716a681ecb25a3c15c2387a8c23714c9510f61b
Suprame4 <adriancortesgonz@gmail.com>
2020-07-09 16:57:30 -0700

    Added JS validation in index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
+const password = document.getElementById("password");
+const password1 = document.getElementById("password1");
 
 const form = document.getElementById("form");
 const errorElement = document.getElementById("error");
@@ -7,16 +9,35 @@ const errorElement = document.getElementById("error");
 form.addEventListener("submit", (e) => {
 
     let messages = []; 
-    if (fname.value === "" || name.value == null){
-        messages.push("Error: First name is required");
+    if (fname.value === "" || fname.value == null){
+        messages.push("ERROR: First name is required");
     }
 
+    if (password.value.length <= 6){
+        messages.push("ERROR: Password must be longer than 6 characters")
+    }
+
+    if(password.value.length >= 20){
+        messages.push("ERROR: Password must be shorter than 6 characters")
+    }
+
+    if (password.value === "password"){
+        messages.push("ERROR: Password cannot be \"password\"")
+
+    }
+      
+    if (password.value !== password1.value){
+        messages.push("ERROR: Password does not match")
+    }
+
+    
     if(messages.length > 0){
 
     e.preventDefault();
-    errorElement.innerText = messages.join(", ");
+    errorElement.innerText = messages.join(",\n ");
 
     }
 })
+
 
 
